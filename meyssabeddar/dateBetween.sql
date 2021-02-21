4c4011e0ebe10a9e22d49532432d8f4149968530
meyssa-b <63784696+meyssa-b@users.noreply.github.com>
2021-02-21 20:53:45 +0100

    Delete meyssabeddar directory

--- meyssabeddar/dateBetween.sql
+++
@@ -1,6 +0,0 @@
-SELECT nom,prenom 
-FROM patient 
-	INNER JOIN consultation 
-		ON id_PATIENT=PATIENT_id_PATIENT 
-WHERE date_consultation 
-	BETWEEN '2020-02-01' AND '2020-03-01'
--- meyssabeddar/innerjoin.sql
+++
@@ -1,7 +0,0 @@
-SELECT * 
-FROM patient 
-	INNER JOIN consultation 
-		ON id_PATIENT=PATIENT_id_PATIENT 
-	INNER JOIN medecin 
-		ON id_MEDECIN=MEDECIN_id_MEDECIN 
-WHERE id_medecin=1
