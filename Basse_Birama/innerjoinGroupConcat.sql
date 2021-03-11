10e8f150eafa3c29be42122ba7396bc2e3362008
docteurport <77400939+docteurport@users.noreply.github.com>
2021-03-12 00:07:38 +0100

    Delete innerjoinGroupConcat.sql

--- Basse_Birama/innerjoinGroupConcat.sql
+++
@@ -1,4 +0,0 @@
-select group_concat(concat(nom_professeur,',' ,prenom_professeur) separator=';') as enseignant
-from professeur as p
-inner join cours_spécifiques as c on p.id_professeur=c.id_professeur
-order by nom_professeur
