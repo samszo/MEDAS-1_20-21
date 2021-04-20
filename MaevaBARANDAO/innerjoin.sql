30b8b69039686fb331ec23674d058e082e52b241
MaevaBARANDAO <73832634+maeeeeeeeeva@users.noreply.github.com>
2021-04-20 10:51:55 +0200

    Delete MaevaBARANDAO directory

--- MaevaBARANDAO/innerjoin.sql
+++
@@ -1,5 +0,0 @@
-SELECT article_demandé, Ref_article
-FROM DA 
-	INNER JOIN concretisation ON Ref_DA=fk_Ref_DA
-	INNER JOIN Commandes ON Ref_commande=fk_Ref_commande
-WHERE adresse_livraison = "ici"
--- MaevaBARANDAO/innerjoinGroupConcat.sql
+++
@@ -1,4 +0,0 @@
-SELECT GROUP_CONCAT(fk_Ref_commande) AS id_besoin, Ref_article
-FROM Commandes
-	INNER JOIN concretisation ON  Ref_commande=fk_Ref_commande
-WHERE adresse_livraison="ici"
