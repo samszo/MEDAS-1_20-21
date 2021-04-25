738cfabe3a32d9191b467139174c53d484d35cb9
TomBlach <71490738+TomBlach@users.noreply.github.com>
2021-04-25 23:27:23 +0200

    Add files via upload

---
+++ Blachon_Tom/omk-query.sql
@@ -0,0 +1,17 @@
+### Afficher la liste des propriétés d'un ressource template
+SELECT pro.*
+FROM resource_template rest
+INNER JOIN resource_template_property restpro
+on rest.id = restpro.resource_template_id
+INNER JOIN property pro
+on restpro.property_id = pro.id
+WHERE rest.label = "classes"
+
+### Afficher les valeurs des propriétés d'un item
+SELECT pro.*, res.*
+FROM property pro
+INNER JOIN value val
+on pro.id = val.property_id
+INNER JOIN resource res
+on val.resource_id = res.id
+WHERE res.title = "Mathématiques"
