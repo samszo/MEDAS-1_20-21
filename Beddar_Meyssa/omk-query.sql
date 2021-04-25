b9224cda32f496f04a8bf2d18144fd78f80b0335
meyssa-b <63784696+meyssa-b@users.noreply.github.com>
2021-04-26 01:49:15 +0200

    Add files via upload

---
+++ Beddar_Meyssa/omk-query.sql
@@ -0,0 +1,17 @@
+-- Requête qui affiche la liste des propriétés du resource template "Medecin"
+SELECT prop.*
+FROM resource_template rt
+INNER JOIN resource_template_property rtp
+    on rt.id = rtp.resource_template_id
+INNER JOIN property prop
+    on rtp.property_id = prop.id
+WHERE rt.label = "Medecin"
+
+-- Requête qui affiche les valeurs des propriétés de l'item "MARTINS Jérémy"
+SELECT val.*
+FROM value val 
+INNER JOIN property prop
+	on prop.id = val.property_id
+INNER JOIN resource res
+    on val.resource_id = res.id
+WHERE res.title = "MARTINS Jérémy"
