a95e746b33247a1a9a7707a1044ec1424b7e8bf7
Peadz <72613969+Peadz@users.noreply.github.com>
2021-04-25 19:55:10 +0200

    Add files via upload

---
+++ SIMOES Matthieu/omk-query.sql
@@ -0,0 +1,19 @@
+-- pour afficher la liste des propriétés d'un ressource template
+SELECT p.*
+FROM resource_template rt
+INNER JOIN resource_template_property rtp
+    on rt.id = rtp.resource_template_id
+INNER JOIN property p
+    on rtp.property_id = p.id
+WHERE rt.label = "Basketteur"
+
+-- pour afficher les valeurs des propriétés d'un item
+
+SELECT p.*
+	,r.*
+FROM property p
+INNER JOIN value v
+    on p.id = v.property_id
+INNER JOIN resource r
+    on v.resource_id = r.id
+WHERE r.title = "Lebron James"
