558d73383bbf5ba141b32d9c5118c1741cca3a40
nassimaoukhelifa <68420410+nassimaoukhelifa@users.noreply.github.com>
2021-04-26 09:33:28 +0200

    Update omk-query.sql

--- Nassima_Oukhelifa/omk-query.sql
+++ Nassima_Oukhelifa/omk-query.sql
@@ -1,4 +1,4 @@
--- pour afficher la liste des propriétés d'un ressource template
+
 SELECT p.*
 FROM property  P INNER JOIN resource_template_property rtp 
 on  p.id = rtp.property_id
@@ -6,14 +6,7 @@ inner join resource_template rt
 on rt.id = rtp.resource_template_id
 WHERE rt.label = "client"
 
--- pour afficher les valeurs des propriétés d'un item
-SELECT  r.title,p.*,r.*
-FROM property p
-INNER JOIN value v
-    on p.id = v.property_id
-INNER JOIN resource r
-    on v.resource_id = r.id
-WHERE r.title  = "Yousfi"
+
 
 
 
