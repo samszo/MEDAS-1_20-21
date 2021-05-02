3318e9ff32f5585d54c6b4c46cbf2ee383fc760d
MaevaBARANDAO <73832634+maeeeeeeeeva@users.noreply.github.com>
2021-05-02 18:23:22 +0200

    Update omk-query.sql

--- BARANDAO_Maeva/omk-query.sql
+++ BARANDAO_Maeva/omk-query.sql
@@ -1,7 +1,11 @@
 -- Affiche les noms des propriétés du resource template Article
 select p.local_name
-from property as p JOIN resource_template_property as rtp ON p.id = rtp.property_id
-where rtp.resource_template_id = (select id from resource_template where label = "Article");
+from property as p 
+  INNER JOIN 
+  resource_template_property as rtp ON p.id = rtp.property_id
+  INNER JOIN 
+  resource_template as rt ON rt.id = rtp.resource_template_id
+where rt.id = 4;
 
 -- donne les valeurs des propriétés d'un item
 select value from value where resource_id = 6; 
