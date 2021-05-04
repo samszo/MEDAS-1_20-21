c6929421a311130e2f41a3ddc0014af9b4b5054b
nassimaoukhelifa <68420410+nassimaoukhelifa@users.noreply.github.com>
2021-05-04 14:45:32 +0200

    Delete 01_nbr_test_04052021.sql

--- Nassima_Oukhelifa/examen/rt/01_nbr_test_04052021.sql
+++
@@ -1,4 +0,0 @@
-/*Ma base de données contient des informations pour la date de l'examen 04/05/2021*/
-/*combien de test sont disponibles aujourd'hui ?*/
-
-select sum(quantite) from stock where date = current_date()
