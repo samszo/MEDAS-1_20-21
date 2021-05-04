a50e7b96fba9c7c250e592163ff1f4f41334d042
nassimaoukhelifa <68420410+nassimaoukhelifa@users.noreply.github.com>
2021-05-04 14:46:20 +0200

    Delete 05_nbr_test_type_04052021.sql

--- Nassima_Oukhelifa/examen/rt/05_nbr_test_type_04052021.sql
+++
@@ -1,7 +0,0 @@
-/*Ma base de données contient des informations pour la date de l'examen 04/05/2021*/
-/*Nombre de test effectué aujourd'hui par type de test ?*/
-select t.id, libelle, count(*) from patient p inner join rdv r on p.id = r.id_patient
-inner join test_covid t on r.id_test = t.id 
- where date = current_date() and effectue = 'O'
- group by t.id,libelle
- 
