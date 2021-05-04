5d2f592ca036375f56a9bf28b749174dd6110c05
Thomas PE <60079895+toto75019@users.noreply.github.com>
2021-05-04 13:11:22 +0200

    Delete ThomasPedinotti directory

--- ThomasPedinotti/innerjoin.sql
+++
@@ -1,2 +0,0 @@
-select regions_commerciales.id_region, agences.id_agence, contrats.id_contrats from mydb.regions_commerciales 
-inner join mydb.agences on regions_commerciales.id_region=agences.iid_region inner join mydb.contrats on agences.id_agence=contrats.agences_id_agence
--- ThomasPedinotti/innerjoinGroupConcat.sql
+++
@@ -1,2 +0,0 @@
-select  group_concat(concat(regions_commerciales.id_region, agences.id_agence, contrats.id_contrats), separator= ' ') from regions_commerciales 
-inner join agences on regions_commerciales.id_region=agences.iid_region inner join contrats on agences.id_agence=contrats.agences_id_agence
